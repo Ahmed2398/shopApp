30ee3136af61e90d86381863872e41c74ff91b3e
Ahmed Ashraf <57802370+Ahmed2398@users.noreply.github.com>
2022-12-06 11:09:57 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -3,20 +3,16 @@ import App from './App.vue'
 import router from './router'
 import { projectAuth } from './firebase/config'
 
-import Carousel3d from 'vue3-carousel-3d';
 import "vue3-carousel-3d/dist/index.css";
 
 
 //global style
-// import './assets/style/main.scss'
-import './assets/style/common.css';
+
 
 
 // bootstrap config 
 
     import "./assets/css/templatemo-liberty-market.css";
-    import "./assets/css/owl.css";
-    import "./assets/css/animate.css";
    
     import "bootstrap/dist/css/bootstrap.css"
 
@@ -31,20 +27,3 @@ projectAuth.onAuthStateChanged(() => {
 
     }   
 })
-// import "bootstrap/dist/js/bootstrap";
-import"./vendor/jquery/jquery.min.js";
-
-// import "./assets/js/isotope.min.js";
-import "./assets/js/owl-carousel.js";
-
-// import "./assets/js/popup.js";
-// import "./assets/js/custom.js";
-let owl_carousel = require('owl.carousel');
-window.fn = owl_carousel;
-
-window.$ = window.jQuery = require('jquery');
-Vue.use({
-    install: function(Vue, options){
-        Vue.prototype.$jQuery = require('jquery'); // you'll have this.$jQuery anywhere in your vue project
-        }
-    });
